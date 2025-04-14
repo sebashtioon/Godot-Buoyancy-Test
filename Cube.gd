a09f430019fcf3f7fb36f09b4bf699e64df59a3a
sebashtioon <sebastiansuciu607@gmail.com>
2025-04-15 00:46:59 +1000

    add some sick clouds

--- Cube.gd
+++ Cube.gd
@@ -11,15 +11,6 @@ extends RigidBody3D
 
 var submerged := false
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-
 func _physics_process(delta):
 	submerged = false
 	for p in probes:
